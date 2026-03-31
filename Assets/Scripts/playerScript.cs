f933559947684eb2392fdc6e48e5afaf63d157a2
Robert Lewis <caysonlewis@gmail.com>
2026-03-31 18:28:21 -0500

    added movement

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -3,8 +3,8 @@ using UnityEngine.InputSystem;
 
 public class playerScript : MonoBehaviour
 {
-    private float speed;
-    public int direction;
+    public float speed;
+    private int direction;
     //1 = up
     //2 = down
     //3 = left
@@ -19,28 +19,47 @@ public class playerScript : MonoBehaviour
     void Update()
     {
         Vector2 newPos = transform.position;
-        if (direction == 1)
-        {
-            newPos.y = transform.position.y + speed;
-        }
-        if ((Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.wKey.wasPressedThisFrame) && direction != 1)
+
+        if ((Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.wKey.wasPressedThisFrame) && direction != 2)
         {
             direction = 1;
         }
 
-        if ((Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current.sKey.wasPressedThisFrame) && direction != 2)
+        if ((Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current.sKey.wasPressedThisFrame) && direction != 1)
         {
             direction = 2;
         }
 
-        if ((Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame) && direction != 3)
+        if ((Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame) && direction != 4)
         {
             direction = 3;
         }
         
-        if ((Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame) && direction != 4)
+        if ((Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame) && direction != 3)
         {
             direction = 4;
         }
+        
+        if (direction == 1)
+        {
+            newPos.y = transform.position.y + speed * Time.deltaTime;
+        }
+        
+        if (direction == 2)
+        {
+            newPos.y = transform.position.y - speed * Time.deltaTime;
+        }
+
+        if (direction == 3)
+        {
+            newPos.x = transform.position.x - speed * Time.deltaTime;
+        }
+        
+        if (direction == 4)
+        {
+            newPos.x = transform.position.x + speed * Time.deltaTime;
+        }
+
+        transform.position = newPos;
     }
 }
